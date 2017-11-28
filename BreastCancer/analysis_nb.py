9e55d75c4871264b6985ffee32386baae89c9d66
bfeldsar <bf47667@fer.hr>
2017-11-28 10:47:03 +0100

    Fixed naive bayes discretization in KDDcup98

--- BreastCancer/analysis_nb.py
+++ BreastCancer/analysis_nb.py
@@ -2,7 +2,7 @@ from sklearn.naive_bayes import MultinomialNB
 import BreastCancer.analysis_utils as utils
 
 if __name__ == '__main__':
-    clf = MultinomialNB()
+    clf = MultinomialNB(alpha=0.1)
     y_true, y_pred = utils.do_classification(clf)
     utils.write_results(y_true, y_pred,"nb")
 
--- KDDcup1998/analysis_svm.py
+++ KDDcup1998/analysis_svm.py
@@ -2,6 +2,6 @@ import KDDcup1998.analysis_utils as utils
 from sklearn.svm import SVC
 
 if __name__ == '__main__':
-    clf = SVC(C=0.5)
+    clf = SVC()
     y_true, y_pred = utils.do_classification(clf)
-    #utils.write_subms(y_true,y_pred,"svm")
+    utils.write_subms(y_true,y_pred,"svm")
